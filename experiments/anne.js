de8c0d4a19a473e5b0e6488967a0c9588e50936e
AKinnander44 <149686770+AntonKinnander@users.noreply.github.com>
2025-09-01 18:21:11 +0200

    Background

--- experiments/anne.js
+++ experiments/anne.js
@@ -6,3 +6,16 @@
 // Basically im thinking i could just create lines along the edges of the canvas, going towards origo, the dark shape in the middle should still appear because close to 0 x or y there will be a huge amount of lines crammed into the space.
 
 
+function setup() {
+    createCanvas(innerWidth, innerHeight);
+    background(15, 0, 152);
+  }
+
+function draw() {
+    colormode(RGB);
+    background(34, 39, 46);
+    fill(108, 182, 255);
+
+    translate(width / 2, height / 2);
+    
+}
