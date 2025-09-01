b4fc966c2765f5e85a5e0225358d61e267d9d49d
AKinnander44 <149686770+AntonKinnander@users.noreply.github.com>
2025-09-01 18:16:15 +0200

    Planning out ANNE.JS

---
+++ experiments/anne.js
@@ -0,0 +1,8 @@
+// P5.js recreation of the Anne by Georg Nees
+
+// Looking at the original it looks like there are lines beginning at the edges of the canvas and extending towards origo (0,0)
+// Also the line density increases as they get closer to the 0 coordinate of their axis, which creates a 3d effect
+
+// Basically im thinking i could just create lines along the edges of the canvas, going towards origo, the dark shape in the middle should still appear because close to 0 x or y there will be a huge amount of lines crammed into the space.
+
+
