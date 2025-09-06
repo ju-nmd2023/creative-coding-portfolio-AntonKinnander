08cbef9ce31fa040ffaac739577d81f7b0acc1df
AKinnander44 <149686770+AntonKinnander@users.noreply.github.com>
2025-09-06 19:50:26 +0200

    Roni

--- experiments/roni.js
+++ experiments/roni.js
@@ -4,38 +4,34 @@
 // 1. Grid of squares - square with full corner radius looks like a circle
 // 2. Spiral pattern - use the mathematical function of a spiral? Then figure out what value i need to control to rotate it
 
+const gap = 5;
+const size = 40;
+const countPerRow = 7;
+
 function setup() {
-  createCanvas(innerWidth, innerHeight);
-  background(34, 39, 46);
-}
+    createCanvas(innerWidth, innerHeight);
+    windowResized();
+    colorMode(RGB);
+    background(250);
+    rectMode(CENTER);
+    noStroke();
+  }
 
 function draw() {
-  background(34, 39, 46, 40);
-  noStroke();
-  fill(108, 182, 255);
-
-  push();
-  translate(width / 2, height / 2);
-
-  push();
-  rotate(frameCount / 8);
-  ellipse(25, 0, 50);
-  pop();
-
-  push();
-  rotate(-frameCount / 10);
-  ellipse(75, 0, 50);
-  pop();
-
-  push();
-  rotate(frameCount / 12);
-  ellipse(125, 0, 50);
-  pop();
-
-  push();
-  rotate(-frameCount / 14);
-  ellipse(175, 0, 50);
-  pop();
-
-  pop();
+ translate(width / 2, height / 2);
+//  center drawing
+ translate(-size * countPerRow / 2, -size * countPerRow / 2);
+ for (let x = 0; x < countPerRow; x++) {
+    for (let y = 0; y < countPerRow; y++) {
+        push();
+        fill(255,60,60);
+        translate(x * (size + gap), y * (size + gap));
+        square(0, 0, size);
+        pop();
+    }
+ }
 }
+
+function windowResized() {
+    resizeCanvas(innerWidth, innerHeight);
+  }
