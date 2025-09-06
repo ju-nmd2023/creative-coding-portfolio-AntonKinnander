7a487f4e3755b86d32c642567c9ed962d8302e58
AKinnander44 <149686770+AntonKinnander@users.noreply.github.com>
2025-09-06 20:04:12 +0200

    how 2 center element

--- experiments/roni.js
+++ experiments/roni.js
@@ -5,7 +5,7 @@
 // 2. Spiral pattern - use the mathematical function of a spiral? Then figure out what value i need to control to rotate it
 
 const gap = 5;
-const size = 40;
+let size;
 const countPerRow = 7;
 
 function setup() {
@@ -15,23 +15,30 @@ function setup() {
     background(250);
     rectMode(CENTER);
     noStroke();
+    
   }
 
 function draw() {
- translate(width / 2, height / 2);
-//  center drawing
- translate(-size * countPerRow / 2, -size * countPerRow / 2);
+//  center canvas
+// translate(-width / 2, -height / 2);
+ 
+//  center drawing 
+beginShape();
+// translate(size * countPerRow + gap * (countPerRow - 1) / 2, size * countPerRow + gap * (countPerRow - 1) / 2);
  for (let x = 0; x < countPerRow; x++) {
     for (let y = 0; y < countPerRow; y++) {
         push();
         fill(255,60,60);
-        translate(x * (size + gap), y * (size + gap));
+        translate(x * (size + (gap)), y * (size + (gap)));
         square(0, 0, size);
         pop();
     }
  }
 }
+endShape();
 
 function windowResized() {
     resizeCanvas(innerWidth, innerHeight);
+    size = min(width, height) / 12;
+    clear();
   }
