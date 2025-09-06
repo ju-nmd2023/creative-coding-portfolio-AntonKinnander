22c6f07b87c34724abd2edcd680d284a95a0afba
AKinnander44 <149686770+AntonKinnander@users.noreply.github.com>
2025-09-06 20:17:04 +0200

    Introduce way to control with spiral size

--- experiments/roni.js
+++ experiments/roni.js
@@ -4,9 +4,10 @@
 // 1. Grid of squares - square with full corner radius looks like a circle
 // 2. Spiral pattern - use the mathematical function of a spiral? Then figure out what value i need to control to rotate it
 
-const gap = 5;
+const gap = 7.5;
 let size;
 const gridSize = 7;
+let spiralFactor = .5;
 
 function setup() {
     createCanvas(innerWidth, innerHeight);
@@ -24,14 +25,15 @@ function draw() {
     
     // Center w/o translate
     const startX = (width - gridWidth) / 2;
-    const startY = (height - gridHeight) / 2;
+    const startY = (height - gridHeight) / 2 - height/30;
     
     for (let x = 0; x < gridSize; x++) {
         for (let y = 0; y < gridSize; y++) {
             push();
             fill(255, 60, 60);
             translate(startX + x * (size + gap), startY + y * (size + gap));
-            square(0, 0, size);
+            
+            square(0, 0, size, size * spiralFactor/(2));
             pop();
         }
     }
@@ -41,4 +43,6 @@ function windowResized() {
     resizeCanvas(innerWidth, innerHeight);
     size = min(width, height) / 12;
     clear();
+    console.log(size, gap);
+    //corner radius max value = size/2
   }
