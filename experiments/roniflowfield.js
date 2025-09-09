da1dc5b46284d9b46abd8718714351480dd6b767
AKinnander44 <149686770+AntonKinnander@users.noreply.github.com>
2025-09-09 12:12:34 +0200

    Kinda

--- experiments/roniflowfield.js
+++ experiments/roniflowfield.js
@@ -1,25 +1,41 @@
-// Recreation of Roni Kaufmans artwork Digital Impressionism featuring rectangles that are placed along flowfields looking a little like a RGB dispplay upclose 
+// Recreation of Roni Kaufmans artwork Digital Impressionism featuring rectangles that are placed along flowfields looking a little like a RGB dispplay upclose
 // FIrst recreation without the particles being animated
 
 const red = [255, 0, 0];
 const green = [0, 255, 0];
 const blue = [0, 0, 255];
 
+const spacing = 10;
+const lineLength = 12;
+const noiseScale = 0.005; //for the perlin nouse
+
 function setup() {
-    createCanvas(innerWidth, innerHeight);
-    windowResized();
-    colorMode(RGB);
-    background(255);
-    // rectMode(CENTER);
-    noStroke();
-    
+  createCanvas(innerWidth, innerHeight); 
+  background(0);
+ 
+  for (let y = 0; y < height; y += spacing) {
+    for (let x = 0; x < width; x += spacing) {
+      
+
+
+      
+      const color = random([red, green, blue]);
+      stroke(color);
+
+      const angle = noise(x * noiseScale, y * noiseScale) * TWO_PI * 2;
+
+      // Calculate start and end points
+      const x1 = x - (lineLength / 2) * cos(angle);
+      const y1 = y - (lineLength / 2) * sin(angle);
+      const x2 = x + (lineLength / 2) * cos(angle);
+      const y2 = y + (lineLength / 2) * sin(angle);
+
+      line(x1, y1, x2, y2);
+    }
   }
-
-function draw() {
-
 }
 
+
 function windowResized() {
-    resizeCanvas(innerWidth, innerHeight);
-    clear();
-  }
+  resizeCanvas(windowWidth, windowHeight);
+}
