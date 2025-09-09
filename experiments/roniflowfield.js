c19062b0946cf5079134bef430195797b6e7a19e
AKinnander44 <149686770+AntonKinnander@users.noreply.github.com>
2025-09-09 12:25:56 +0200

    V1

--- experiments/roniflowfield.js
+++ experiments/roniflowfield.js
@@ -5,28 +5,39 @@ const red = [255, 0, 0];
 const green = [0, 255, 0];
 const blue = [0, 0, 255];
 
-const spacing = 10;
+const spacing = 12;
 const lineLength = 12;
 const noiseScale = 0.005; //for the perlin nouse
 
 function setup() {
   createCanvas(innerWidth, innerHeight); 
   background(0);
+
+  //Canvas size
+  const w = width/3;
+  const h = w * (9/16);
+  const x = (width - w) / 2;
+  const y = (height - h) / 2;
+
+  //Center
+  translate(x, y);
  
-  for (let y = 0; y < height; y += spacing) {
-    for (let x = 0; x < width; x += spacing) {
+  for (let y = 0; y < h; y += spacing * (1 + Math.random())) { 
+    for (let x = 0; x < w; x += spacing * (1 + Math.random())) {
       
 
       const color = random([red, green, blue]);
       stroke(color);
+      strokeWeight(3);
+      strokeCap(SQUARE);
 
       const angle = noise(x * noiseScale, y * noiseScale) * TWO_PI * 2;
 
       // Calculate start and end points
-      const x1 = x - (lineLength / 2) * cos(angle * Math.random()) ;
-      const y1 = y - (lineLength / 2) * sin(angle * Math.random()) ;
-      const x2 = x + (lineLength / 2) * cos(angle * Math.random()) ;
-      const y2 = y + (lineLength / 2) * sin(angle * Math.random()) ;
+      const x1 = x - (lineLength / 2) * cos(angle);
+      const y1 = y - (lineLength / 2) * sin(angle);
+      const x2 = x + (lineLength / 2) * cos(angle);
+      const y2 = y + (lineLength / 2) * sin(angle);
 
       line(x1, y1, x2, y2);
     }
